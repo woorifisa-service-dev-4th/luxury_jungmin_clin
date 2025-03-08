a69743198f50eb8d38e5c5b76f789e2d9cea216a
mjgwon24 <alswlchlrh8@naver.com>
2025-03-09 02:51:42 +0900

    feature: 주인 및 연관 펫, 기록 조회 - mybatis 쿼리 매핑을 위한 Mapper 인터페이스 및 xml 구현

---
+++ src/main/java/dev/spring/petclinic/searchResult/mapper/OwnersMapper.java
@@ -0,0 +1,11 @@
+package dev.spring.petclinic.searchResult.mapper;
+
+import dev.spring.petclinic.searchResult.model.Owners;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface OwnersMapper {
+    // ownerId 기반으로 Owner 조회
+    Owners selectOwnerById(int id);
+}
