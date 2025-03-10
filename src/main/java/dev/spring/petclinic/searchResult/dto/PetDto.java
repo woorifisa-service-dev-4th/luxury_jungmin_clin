10703e71f30b911d13d0320c405b87c7192c4621
mjgwon24 <alswlchlrh8@naver.com>
2025-03-10 09:41:44 +0900

    update: Pet model, dto 기본 생성자, 모든 필드 인자로 받는 생성자 어노테이션 추가

--- src/main/java/dev/spring/petclinic/searchResult/dto/PetDto.java
+++ src/main/java/dev/spring/petclinic/searchResult/dto/PetDto.java
@@ -1,19 +1,25 @@
 package dev.spring.petclinic.searchResult.dto;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Builder
+@Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PetDto {
     private int id;
     private int ownerId;
     private String name;
     private LocalDate birthDate;
-    private String type;
+    private int typeId;
 
     private List<PetVisitDto> visits;
+
+    public void assignOwner(int ownerId) {
+        this.ownerId = ownerId;
+    }
 }
--- src/main/java/dev/spring/petclinic/searchResult/model/Pets.java
+++ src/main/java/dev/spring/petclinic/searchResult/model/Pets.java
@@ -1,18 +1,23 @@
 package dev.spring.petclinic.searchResult.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pets {
     private int id;
     private int ownerId;
     private String name;
     private LocalDate birthDate;
-    private String type;
-
+    private int typeId;
 
     private List<Visits> visits;
 }
